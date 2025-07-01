a0886428c92345947d275e9696b939ddef66f917
the_lost_dev <elselwyn@gmail.com>
2025-07-01 01:41:19 +0100

    refactor: enhance MainAppBar icon handling and adjust icon size for better usability

--- lib/src/paystack_pay_now.dart
+++ lib/src/paystack_pay_now.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_typing_uninitialized_variables, use_build_context_synchronously
 
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -265,14 +266,18 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
       centerTitle: true,
       automaticallyImplyLeading: false,
       backgroundColor: backgroundColor,
-      surfaceTintColor:Colors.red,
+      surfaceTintColor: Colors.red,
       //  . const Color(0xFFF5F5F5),
       title: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Row(
           children: [
             IconButton(
-              icon: const Icon(Icons.home_outlined),
+              iconSize: 18,
+              color: Colors.black,
+              icon: Platform.isIOS
+                  ? const Icon(Icons.arrow_back_ios_new)
+                  : const Icon(Icons.arrow_back),
               onPressed: () {
                 Navigator.of(context).pop();
               },
